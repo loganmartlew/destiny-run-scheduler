31fc0c8da5f350471b09ec0ba0f54b958cdb8074
Logan Martlew <logan.martlew@gmail.com>
2021-03-06 17:22:35 +1300

    Initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../styles/theme';
+import 'fontsource-roboto';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline />
+      <Component {...pageProps} />
+      {/* </ThemeProvider> */}
+    </>
+  );
+};
 
-export default MyApp
+export default App;
